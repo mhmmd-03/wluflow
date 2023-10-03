09c70737d3ead0e7633db12ef4eca8b2d1b9eb59
Faizaan Qureshi <qfaizaan@gmail.com>
2023-10-02 22:13:53 -0400

    added functionality

--- backendplayground.js
+++ backendplayground.js
@@ -1,53 +1,36 @@
 const axios = require('axios');
 const puppeteer = require('puppeteer');
 
-//let cookies = null;
+const apiUrl = 'https://loris.wlu.ca/register/ssb/registration/registration';
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://loris.wlu.ca/register/ssb/courseSearch/courseSearch');
-    const cookie = await page.cookies();
-    await browser.close();
-    for (i in cookie) {
-        console.log(cookie[i].name + '=' + cookie[i].value);
+    await page.setViewport({ width: 1800, height: 900 });
+    await page.goto(apiUrl);
+    await page.waitForSelector('#catalogSearchLink');
+    await page.click('#catalogSearchLink');
+    await page.waitForSelector('a.select2-choice');
+    await page.click('a.select2-choice');
+    await page.waitForTimeout(1000);
+    await page.keyboard.press('Enter');
+    await page.click('button#term-go');
+    await page.waitForTimeout(1000);
+    await page.click('button#search-go');
+    await page.waitForTimeout(1000);
+    let buttons = await page.$$('table#table1 button.form-button.search-section-button');
+
+    //await buttons[0].click();
+
+    // Output the text content of each button
+    let i = 0
+    while (i < buttons.length) {
+        await page.waitForTimeout(1000);
+        await buttons[i].click();
+        await page.waitForTimeout(1000);
+        await page.click('a.form-button.return-course-button');
+        await page.waitForTimeout(1000);
+        buttons = await page.$$('table#table1 button.form-button.search-section-button')
+        i++;
     }
 })();
-
-/*
-
-const apiUrl = 'https://loris.wlu.ca/register/ssb/courseSearchResults/courseSearchResults';
-
-const headers = {
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Cache-Control': 'max-age=0',
-    'Connection': 'keep-alive',
-    'Host': 'loris.wlu.ca',
-    'Upgrade-Insecure-Requests': '1',
-};
-
-const cookies = [
-    'f5avraaaaaaaaaaaaaaaa_session_=EJDIILIKLKFGDNFLAHFNOOPHGMKKNMBKEOICLODOEODBJNECHPNPCLKINNAKKAOIOBEDOFNJJHKGANLAEOEAMJHDPJMFIGMACHHOFKMKIPLBMPNBPLFBHPKENKOILMNN',
-    'f5_cspm=1234',
-    'JSESSIONID=2ACC264D7FFFDFBDACAD453365407D75',
-    'BIGipServerpool_prodlorisregister=1079380490.24353.0000',
-    'BIGipServerpool_prodlorisbanextension=1029048842.18213.0000'
-];
-
-const axiosInstance = axios.create({
-    headers: {
-        ...headers,
-        Cookie: cookies.join('; '),
-    },
-});
-
-axiosInstance.get(apiUrl)
-    .then((response) => {
-        console.log(response.data);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-
-*/
