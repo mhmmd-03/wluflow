b5dae90700d72265d250967d77b7f03194223e9c
Faizaan Qureshi <qfaizaan@gmail.com>
2023-10-02 23:17:07 -0400

    added functions for requests, added qs module for stringify

--- backendplayground.js
+++ backendplayground.js
@@ -1,11 +1,16 @@
 const axios = require('axios');
 const puppeteer = require('puppeteer');
+const qs = require('qs');
 
 const apiUrl = 'https://loris.wlu.ca/register/ssb/registration/';
-const getCourseInfo1 = 'https://loris.wlu.ca/register/ssb/searchResults/searchResults?txt_subjectcoursecombo=BU111&txt_term=202405&pageOffset=0&pageMaxSize=10&sortColumn=subjectDescription&sortDirection=asc';
-const getCourseInfo2 = 'https://loris.wlu.ca/register/ssb/searchResults/searchResults?txt_subjectcoursecombo=BU231&txt_term=202405&pageOffset=0&pageMaxSize=10&sortColumn=subjectDescription&sortDirection=asc';
+const getCourseInfo1 = 'https://loris.wlu.ca/register/ssb/searchResults/searchResults?txt_subjectcoursecombo=BU111&txt_term=202405&pageOffset=0&pageMaxSize=50&sortColumn=subjectDescription&sortDirection=asc';
+const getCourseInfo2 = 'https://loris.wlu.ca/register/ssb/searchResults/searchResults?txt_subjectcoursecombo=BU231&txt_term=202405&pageOffset=0&pageMaxSize=50&sortColumn=subjectDescription&sortDirection=asc';
 const getCourses = 'https://loris.wlu.ca/register/ssb/courseSearchResults/courseSearchResults?txt_term=202405&startDatepicker=&endDatepicker=&uniqueSessionId=q87811696297527941&pageOffset=0&pageMaxSize=10&sortColumn=subjectDescription&sortDirection=asc';
+const courseSearchURL = 'https://loris.wlu.ca/register/ssb/searchResults/searchResults';
 
+// getCookies(page) takes a page from a puppeteer browser, and performs certain actions to get to the required LORIS pages.
+// It then returns all browser cookies and returns them in a list
+// Usage: getCookies(page)
 async function getCookies(page) {
     await page.goto(apiUrl);
     await page.waitForSelector('#catalogSearchLink');
@@ -30,6 +35,51 @@ async function getCookies(page) {
     ];
     return cookies;
 }
+//
+
+
+// reset(axiosInstance) takes an axios axiosInstance, and resets the data form. This operation must be performed so that
+// subsequent requests to get course information work as expected
+// Usage: reset(axiosInstance)
+async function reset(axiosInstance) {
+    try {
+        await axiosInstance.post('https://loris.wlu.ca/register/ssb/courseSearch/resetDataForm', 'resetCourses=false&resetSections=true')
+    } catch (error) {
+        console.error('Reset failed:', error);
+    }
+}
+//
+
+// getCourseInfo(courseCode, term, axiosInstance) takes in a string courseCode, string term, and axios axiosInstance.
+// It initializes a payload accordingly, sends a request to get course information, and returns the response data.
+// Usage: getCourseInfo('BU121', '202405', axiosInstance);
+async function getCourseInfo(courseCode, term, axiosInstance) {
+    await reset(axiosInstance);
+    const payload = {
+        txt_subjectcoursecombo: courseCode,
+        txt_term: term,
+        pageOffset: 0,
+        pageMaxSize: 50,
+        sortColumn: 'subjectDescription',
+        sortDirection: 'asc'
+    }
+
+    const payloadString = qs.stringify(payload);
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    };
+
+    try {
+        const response = await axiosInstance.post(courseSearchURL, payloadString, config);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+//
 
 (async () => {
     const browser = await puppeteer.launch({ headless: true });
@@ -43,25 +93,11 @@ async function getCookies(page) {
         },
     });
 
-
-    axiosInstance.post('https://loris.wlu.ca/register/ssb/courseSearch/resetDataForm', 'resetCourses=false&resetSections=true')
-        .then((response) => {
-            console.log(response.data);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-
-    axiosInstance.get(endpointUrl3)
-        .then((response) => {
-            console.log(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-
-
+    await getCourseInfo('BU111', '202405', axiosInstance);
+    await getCourseInfo('BU121', '202405', axiosInstance);
     /*
+
+    
     axiosInstance.get(endpointUrl)
         .then((response) => {
             console.log(response.data);
